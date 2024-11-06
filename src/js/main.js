fcbfb5489710ec4f03417943524cdaa0dc080ac4
Никита Федоренко <FedorenkoNA@og-ti.ru>
2024-11-06 18:48:18 +0500

    доработал js файл, лодер появляется строго на 1 секунду

--- src/js/main.js
+++ src/js/main.js
@@ -3,15 +3,15 @@ document.addEventListener('DOMContentLoaded', function() {
 
     buttons.forEach((button, index) => {
         const buttonState = localStorage.getItem(`buttonState${index}`);
-            if (buttonState === 'purchased') {
-                button.innerHTML = "Купить";
-                button.classList.remove('in-cart');
-                localStorage.removeItem(`buttonState${index}`);
-            } else {
-                button.innerHTML = "✔ В корзине";
-                button.classList.add('in-cart');
-                localStorage.setItem(`buttonState${index}`, 'purchased');
-            }
+        if (buttonState === 'purchased') {
+            button.innerHTML = "✔ В корзине";
+            button.classList.add('in-cart');
+            localStorage.setItem(`buttonState${index}`, 'purchased');
+        } else {
+            button.innerHTML = "Купить";
+            button.classList.remove('in-cart');
+            localStorage.removeItem(`buttonState${index}`);
+        }
     });
 });
 
@@ -27,18 +27,22 @@ document.addEventListener('htmx:afterRequest', function(evt) {
     const loader = button.nextElementSibling;
     const index = Array.from(document.querySelectorAll('.buyButton')).indexOf(button);
     const response = evt.detail.xhr.response;
-    if (response) {
-        const buttonState = localStorage.getItem(`buttonState${index}`);
-        if (buttonState === 'purchased') {
-            button.innerHTML = "Купить";
-            button.classList.remove('in-cart');
-            localStorage.removeItem(`buttonState${index}`);
-        } else {
-            button.innerHTML = "✔ В корзине";
-            button.classList.add('in-cart');
-            localStorage.setItem(`buttonState${index}`, 'purchased');
+
+    setTimeout(() => {
+        if (response) {
+            const buttonState = localStorage.getItem(`buttonState${index}`);
+            if (buttonState === 'purchased') {
+                button.innerHTML = "Купить";
+                button.classList.remove('in-cart');
+                localStorage.removeItem(`buttonState${index}`);
+            } else {
+                button.innerHTML = "✔ В корзине";
+                button.classList.add('in-cart');
+                localStorage.setItem(`buttonState${index}`, 'purchased');
+            }
         }
-    }
-    loader.style.display = 'none';
-    button.style.display = 'block';
+        loader.style.display = 'none';
+        button.style.display = 'block';
+    }, 1000); 
 });
+
